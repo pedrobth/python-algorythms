852beb40fba8a252e733ace809c5a1077e6a3a86
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-31 13:43:05 -0300

    feat: anagrams and different aproaches test

---
+++ challenges/challenge_anagrams.py
@@ -0,0 +1,189 @@
+from typing import Counter
+
+
+"""Although to run a serious benchmark the machine should run ONLY the program,
+the execution times commented bellow might represent
+a comparison of those algorithms"""
+
+
+#  0.121676 ~ 0.122801 seconds
+def remove_chars_list(first_string, second_string):
+    # https://github.com/tryber/sd-07-project-algorithms/tree/MoisesSantana-Algorithms
+    first_list = list(first_string)
+    second_list = list(second_string)
+    for letter in first_list:
+        try:
+            index = second_list.index(letter)
+        except ValueError:
+            return False
+
+        second_list.remove(second_list[index])
+
+    return True
+
+# 0.147181 ~  0.15807
+def generate_counter_dict(first_str, second_str):
+    return gen_dict(list(first_str)) == gen_dict(list(second_str))
+
+# 0.147181 ~  0.15807
+def gen_dict(lista):
+    dic = {}
+
+    for letra in lista:
+        if letra in dic:
+            dic[letra] += 1
+        else:
+            dic[letra] = 1
+
+    return dic
+
+# 0.165924 ~ 0.168763 seconds
+def recursive_dict(first_str, second_str):
+    return str_to_dict_recusrsive(first_str, {}) == str_to_dict_recusrsive(second_str, {})
+# 0.165924 ~ 0.168763 seconds
+def str_to_dict_recusrsive(string, str_dict={}):
+    if len(string) == 0:
+        return str_dict
+    str_dict[string[0]] = string.count(string[0])
+    return str_to_dict_recusrsive(string.replace(string[0], ''), str_dict)
+
+
+# 0.163752 ~ 0.184378
+def slow_gen_counter_dict(first_string, second_string):
+    return slow_str_to_dict(list(first_string)) == slow_str_to_dict(list(second_string))
+# 0.163752 ~ 0.184378
+def slow_str_to_dict(lista):
+    dic = {letra: 0 for letra in lista}
+
+    for letra in lista:
+        dic[letra] += 1
+
+    return dic
+
+
+#  0.21519 ~ 0.21801 seconds
+def remove_chars(first_string, second_string):
+    for letter in first_string:
+        try:
+            index = second_string.index(letter)
+        except ValueError:
+            return False
+
+        second_string = second_string[:index] + second_string[index + 1:]
+
+    return True
+
+
+# 0.375300 ~ 0.386072 seconds
+def slow_dict(f_str, s_str):
+    first_dict = {i: f_str.count(i) for i in f_str}
+    second_dict = {a: s_str.count(a) for a in s_str}
+    return first_dict == second_dict
+
+
+# 2.41763 ~ 2.47492 seconds
+def quicksort(first_string, second_string):
+    return quicksort_str(
+        list(first_string), 0, len(first_string) - 1
+    ) == quicksort_str(
+        list(second_string), 0, len(second_string) - 1
+    )
+# pivot for quicksort
+def partition(array, low, high):
+    # https://github.com/tryber/sd-07-project-algorithms/tree/luciano-berchon-project-algorithms
+    i = low - 1
+    pivot = array[high]
+
+    for j in range(low, high):
+
+        if array[j] <= pivot:
+
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+# 2.41763 ~ 2.47492 seconds
+def quicksort_str(array, low, high):
+    # https://github.com/tryber/sd-07-project-algorithms/tree/luciano-berchon-project-algorithms
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition_index = partition(array, low, high)
+
+        quicksort_str(array, low, partition_index - 1)
+        quicksort_str(array, partition_index + 1, high)
+
+    return array
+
+
+def selection_sort(first_string, second_string):
+    return selection_sort_str(first_string) == selection_sort_str(second_string)
+# 5.7930 ~ 5.82011 seconds
+def selection_sort_str(string):
+    str_list = list(string)
+    for i in range(len(str_list)):
+        min_index = i
+        for j in range(i + 1, len(str_list)):
+            if str_list[min_index] > str_list[j]:
+                min_index = j
+        str_list[i], str_list[min_index] = str_list[min_index], str_list[i]
+    return str_list
+
+
+def is_anagram(first_string, second_string):
+    if (
+        not isinstance(first_string, str)
+        or not isinstance(second_string, str)
+        or len(first_string) != len(second_string)
+    ):
+        return False
+
+    # Counter py method turned to be the fastest - I am not surprised at all
+    # 0.076248 ~ 0.09043 seconds
+    return Counter(first_string) == Counter(second_string)
+
+    #  0.121676 ~ 0.122801 seconds
+    # return remove_chars_list(first_string, second_string)
+
+    # 0.133657 ~ 0.137959
+    """# return s o r t e d(
+        list(first_string)) == list(second_string).s o r t ()"""
+
+    # 0.147181 ~  0.15807
+    """return generate_counter_dict(
+        list(first_string)
+    ) == generate_counter_dict(
+        list(second_string)
+    )"""
+
+    # 0.174338 ~ 0.17686 seconds
+    """return recursive_dict(
+        second_string, {}
+    ) == recursive_dict(
+        first_string, {}
+    )"""
+
+    # 0.163752 ~ 0.184378
+    """return slow_gen_counter_dict(
+        list(first_string)
+    ) == slow_gen_counter_dict(
+        list(second_string)
+    )"""
+
+    #  0.21519 ~ 0.21801 seconds
+    # return remove_chars(first_string, second_string)
+
+    # 0.375300 ~ 0.386072 seconds
+    # return slow_dict(first_string, second_string)
+
+    #  2.41763 ~ 2.47492 seconds
+    # return quicksort(
+    #      list(first_string), 0, len(first_string) - 1
+    #  ) == quicksort(
+    #      list(second_string), 0, len(second_string) - 1
+    #  )
+
+    # 5.7930 ~ 5.82011 seconds
+    # return selection_sort(first_string) == selection_sort(second_string)
---
+++ tests/test_anagrams_speed.py
@@ -0,0 +1,141 @@
+from challenges.challenge_anagrams import is_anagram, remove_chars_list, generate_counter_dict, recursive_dict, slow_gen_counter_dict, remove_chars, slow_dict, quicksort, selection_sort
+import timeit
+
+def test_compare_times():
+    setup_import = "from challenges.challenge_anagrams" " import is_anagram"
+    setup_Counter = "from challenges.challenge_anagrams" " import is_anagram"
+    setup_remove_chars_list = "from challenges.challenge_anagrams" " import remove_chars_list"
+    setup_generate_counter_dict = "from challenges.challenge_anagrams" " import generate_counter_dict"
+    setup_recursive_dict = "from challenges.challenge_anagrams" " import recursive_dict"
+    setup_slow_gen_counter_dict = "from challenges.challenge_anagrams" " import slow_gen_counter_dict"
+    setup_remove_chars = "from challenges.challenge_anagrams" " import remove_chars"
+    setup_slow_dict = "from challenges.challenge_anagrams" " import slow_dict"
+    setup_quicksort = "from challenges.challenge_anagrams" " import quicksort"
+    setup_selection_sort = "from challenges.challenge_anagrams" " import selection_sort"
+
+    first_string = (
+        "Lorem ipsum dolor sit amer, consectetur "
+        "adipiscing elit, do sed eiusmod tempor "
+        "incididint ut labore et dolore magna aliqua"
+    )
+
+    second_string = (
+        "Lorem ipsum dolor sit amer, consectetur "
+        "adipiscing elit, do sed eiusmod tempor "
+        "incididint ut labore et dolore magna aliqua"
+    )
+
+    algorithms_correct = is_anagram(first_string, second_string) is True
+    time_is_anagram = timeit.timeit(
+        f'is_anagram("{first_string}", "{second_string}")',
+        setup=f"{setup_import}",
+        number=10000,
+    )
+    correct_time = time_is_anagram <= 0.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_is_anagram}, algoritmo correto? {algorithms_correct}"
+
+    algorithms_correct = remove_chars_list(first_string, second_string) is True
+    time_remove_chars_list = timeit.timeit(
+        f'remove_chars_list("{first_string}", "{second_string}")',
+        setup=f"{setup_remove_chars_list}",
+        number=10000,
+    )
+    correct_time = time_remove_chars_list <= 0.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_remove_chars_list}, algoritmo correto? {algorithms_correct}"
+
+    algorithms_correct = generate_counter_dict(first_string, second_string) is True
+    time_generate_counter_dict = timeit.timeit(
+        f'generate_counter_dict("{first_string}", "{second_string}")',
+        setup=f"{setup_generate_counter_dict}",
+        number=10000,
+    )
+    correct_time = time_generate_counter_dict <= 0.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_generate_counter_dict}, algoritmo correto? {algorithms_correct}"
+
+
+    algorithms_correct = recursive_dict(first_string, second_string) is True
+    time_recursive_dict = timeit.timeit(
+        f'recursive_dict("{first_string}", "{second_string}")',
+        setup=f"{setup_recursive_dict}",
+        number=10000,
+    )
+    correct_time = time_recursive_dict <= 0.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_recursive_dict}, algoritmo correto? {algorithms_correct}"
+
+
+    algorithms_correct = slow_gen_counter_dict(first_string, second_string) is True
+    time_slow_gen_counter_dict = timeit.timeit(
+        f'slow_gen_counter_dict("{first_string}", "{second_string}")',
+        setup=f"{setup_slow_gen_counter_dict}",
+        number=10000,
+    )
+    correct_time = time_slow_gen_counter_dict <= 0.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_slow_gen_counter_dict}, algoritmo correto? {algorithms_correct}"
+
+
+    algorithms_correct = remove_chars(first_string, second_string) is True
+    time_remove_chars = timeit.timeit(
+        f'remove_chars("{first_string}", "{second_string}")',
+        setup=f"{setup_remove_chars}",
+        number=10000,
+    )
+    correct_time = time_remove_chars <= 7.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_remove_chars}, algoritmo correto? {algorithms_correct}"
+
+    algorithms_correct = slow_dict(first_string, second_string) is True
+    time_slow_dict = timeit.timeit(
+        f'slow_dict("{first_string}", "{second_string}")',
+        setup=f"{setup_slow_dict}",
+        number=10000,
+    )
+    correct_time = time_slow_dict <= 7.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_slow_dict}, algoritmo correto? {algorithms_correct}"
+
+
+    algorithms_correct = quicksort(first_string, second_string) is True
+    time_quicksort = timeit.timeit(
+        f'quicksort("{first_string}", "{second_string}")',
+        setup=f"{setup_quicksort}",
+        number=10000,
+    )
+    correct_time = time_quicksort <= 7.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_quicksort}, algoritmo correto? {algorithms_correct}"
+
+    algorithms_correct = selection_sort(first_string, second_string) is True
+    time_selection_sort = timeit.timeit(
+        f'selection_sort("{first_string}", "{second_string}")',
+        setup=f"{setup_selection_sort}",
+        number=10000,
+    )
+    correct_time = time_selection_sort <= 7.2
+    assert (
+        algorithms_correct and correct_time
+    ), f"Falhou, o tempo foi: {time_selection_sort}, algoritmo correto? {algorithms_correct}"
+
+    print(f'\ntime execution for        Counter() from typing :  {time_is_anagram}')
+    print(f'time execution for             remove_chars_list:  {time_remove_chars_list}')
+    print(f'time execution for         generate_counter_dict:  {time_generate_counter_dict}')
+    print(f'time execution for                recursive_dict:  {time_recursive_dict}')
+    print(f'time execution for         slow_gen_counter_dict:  {time_slow_gen_counter_dict}')
+    print(f'time execution for                  remove_chars:  {time_remove_chars}')
+    print(f'time execution for                     slow_dict:  {time_slow_dict}')
+    print(f'time execution for                     quicksort:  {time_quicksort}')
+    print(f'time execution for                selection_sort:  {time_selection_sort}')
+
+
