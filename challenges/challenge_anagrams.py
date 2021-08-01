f455cbf13f1e017a3ec9d4859603bfcf2e83b4c5
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-01 07:47:26 -0300

    feat: merge sort add

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -117,6 +117,41 @@ def quicksort_str(array, low, high):
     return array
 
 
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+def m_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+
+    left_list = m_sort(string[:mid])
+    right_list = m_sort(string[mid:])
+
+    return merge(left_list, right_list)
+def merge_sort(first_string, second_string):
+    return m_sort(list(first_string)) == m_sort(list(second_string))
 
 # 3.419441 seconds complexity O(nlog(n))
 def heapify(str_list, heap_size, root_index):
